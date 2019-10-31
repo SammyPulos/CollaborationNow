85e9752df546ba0ff01ca45df60f379afd296d59
scuPulos <44333074+scuPulos@users.noreply.github.com>
2019-10-30 22:26:39 -0700

    initial commit

---
+++ app/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember_me = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Name', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	#Note that I am not checking and enforcing usernames to be unique
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('There is already an account registered to the provided email.')
