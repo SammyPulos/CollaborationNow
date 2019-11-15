7036916bebeb31aa636312936ff062ececd2639c
Jennifer Young <jtyoung@Jennifers-MacBook-Pro-3.local>
2019-11-14 17:17:32 -0800

    notification update

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from app.models import Listing
 from app.forms import MessageForm
 from app.models import Message
+from app.models import Notification
 
 @app.before_request
 def before_request():
@@ -129,6 +130,7 @@ def send_message(recipient):
         msg = Message(sender=current_user, recipient=user,
                       body=form.message.data)
         db.session.add(msg)
+        user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
         flash('Your message has been sent.')
         return redirect(url_for('user', username=recipient))
@@ -139,6 +141,7 @@ def send_message(recipient):
 @login_required
 def messages():
     current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
     db.session.commit()
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
@@ -150,3 +153,15 @@ def messages():
         if messages.has_prev else None
     return render_template('messages.html', messages=messages.items,
                            next_url=next_url, prev_url=prev_url)
+
+@app.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(
+        Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])
--- colabNow.py
+++ colabNow.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, Listing
+from app.models import User, Listing, Notification, Message
 
 @app.shell_context_processor
 def make_shell_context():
-	return {'db': db, 'User': User, 'Listing': Listing}
+	return {'db': db, 'User': User, 'Listing': Listing, 'Message': Message, 'Notification': Notification}
